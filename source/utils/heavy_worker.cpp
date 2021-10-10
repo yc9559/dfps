5788763c5b4f0c909045472fa41078eb25c1b300
Matt Yang <yccy@outlook.com>
2021-10-10 19:43:17 +0800

    utils: Add heavy worker
    Signed-off-by: Matt Yang <yccy@outlook.com>

---
+++ source/utils/heavy_worker.cpp
@@ -0,0 +1,83 @@
+/*
+Dfps
+Copyright (C) 2021 Matt Yang(yccy@outlook.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "heavy_worker.h"
+#include "fmt_exception.h"
+#include <sys/prctl.h>
+
+DECLARE_SINGLETON(HeavyWorker)
+
+constexpr HeavyWorker::Handle EMPTY_HANDLE = -1;
+
+HeavyWorker::HeavyWorker() {
+    th_ = std::thread([this]() mutable {
+        Work work;
+        Handle cur;
+        Handle prevHandle = EMPTY_HANDLE;
+        for (;;) {
+            {
+                std::unique_lock<std::mutex> lk(mut_);
+                cv_.wait(lk, [this]() {
+                    for (const auto &[_1, w] : works_) {
+                        if (w) {
+                            return true;
+                        }
+                    }
+                    return false;
+                });
+            }
+            cur = EMPTY_HANDLE;
+            for (auto &[name, w] : works_) {
+                cur++;
+                {
+                    std::lock_guard<std::mutex> lk(mut_);
+                    if (w == nullptr) {
+                        continue;
+                    }
+                    work = w;
+                    w = nullptr;
+                }
+                if (cur != prevHandle) {
+                    prctl(PR_SET_NAME, name.c_str());
+                }
+                prevHandle = cur;
+                work();
+            }
+        }
+    });
+}
+
+HeavyWorker::Handle HeavyWorker::Create(const std::string &name) {
+    Handle handle = works_.size();
+    works_.emplace_back(std::make_tuple(name, nullptr));
+    return handle;
+}
+
+void HeavyWorker::SetWork(Handle handle, const Work &work) {
+    if (handle < 0 || handle >= works_.size()) {
+        throw FmtException("HeavyWorker handle not found, handle = {}", handle);
+    }
+
+    auto &[_1, w] = works_[handle];
+    std::unique_lock<std::mutex> lk(mut_);
+    w = work;
+    if (work) {
+        lk.unlock();
+        cv_.notify_one();
+    }
+}
