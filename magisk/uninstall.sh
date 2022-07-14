41d42f32e71079b362fd977f14e8c88004168427
Matt Yang <yccy@outlook.com>
2022-07-14 22:08:09 +0800

    install: Add uninstall script
    Signed-off-by: Matt Yang <yccy@outlook.com>

---
+++ magisk/uninstall.sh
@@ -0,0 +1,49 @@
+#!/system/bin/sh
+#
+# Copyright (C) 2021-2022 Matt Yang
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+USER_PATH=/sdcard/Android/yc/dfps
+
+wait_until_login() {
+    # in case of /data encryption is disabled
+    while [ "$(getprop sys.boot_completed)" != "1" ]; do
+        sleep 1
+    done
+
+    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
+    local test_file="/sdcard/Android/.PERMISSION_TEST"
+    true >"$test_file"
+    while [ ! -f "$test_file" ]; do
+        true >"$test_file"
+        sleep 1
+    done
+    rm "$test_file"
+}
+
+on_remove() {
+    wait_until_login
+
+    # keep user perapp config
+    cp -af $USER_PATH/dfps.txt /sdcard/
+    rm -rf $USER_PATH
+    mkdir -p $USER_PATH
+    mv /sdcard/dfps.txt $USER_PATH/
+
+    cmd settings delete system min_refresh_rate
+}
+
+# do not block boot
+(on_remove &)
